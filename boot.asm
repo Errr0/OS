1159113abca30e0ce81a87868888d1c59e0703cc
Errr0 <fatalerror404.6@gmail.com>
2024-05-30 13:29:27 +0200

    printing strings

--- boot.asm
+++ boot.asm
@@ -1,25 +1,20 @@
+[org 0x7c00]
 mov ah, 0x0e
-mov al, 65
-int 0x10
+mov bx, string
 
-low:
-    inc al
-    cmp al, 91
-    je exit
-    add al, 32
-    int 0x10
-    jmp high
-
-
-high:
-    sub al, 32
-    inc al
-    cmp al, 91
+print:
+    mov al, [bx]
+    cmp al, 0
     je exit
     int 0x10
-    jmp low
+    inc bx
+    jmp print
 
 exit:
     jmp $
+
+string:
+    db "Hello World", 0
+
 times 510-($-$$) db 0
 db 0x55, 0xaa
