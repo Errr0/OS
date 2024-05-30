62a172eec15809480c0233cdd51de620ae83debf
Errr0 <fatalerror404.6@gmail.com>
2024-05-30 13:15:34 +0200

    alternating alphabet

--- boot.asm
+++ boot.asm
@@ -1,6 +1,25 @@
 mov ah, 0x0e
-mov al, 'H'
+mov al, 65
 int 0x10
-jmp $
+
+low:
+    inc al
+    cmp al, 91
+    je exit
+    add al, 32
+    int 0x10
+    jmp high
+
+
+high:
+    sub al, 32
+    inc al
+    cmp al, 91
+    je exit
+    int 0x10
+    jmp low
+
+exit:
+    jmp $
 times 510-($-$$) db 0
 db 0x55, 0xaa
