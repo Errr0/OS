700791c4c06a6d2338d719230daae3be59b04756
Errr0 <fatalerror404.6@gmail.com>
2024-05-31 11:56:38 +0200

    trash doesnt work

--- boot.asm
+++ boot.asm
@@ -1,38 +1,45 @@
-;[org 0x7c00]
+[org 0x7c00]
 string:
-    db 0
-    mov
+    times 5 db 0
+    mov cl, '0'
 input:
     mov bx, string
     mov ah, 0x00
     int 0x16
-    mov ah, 0x0e
-    int 0x10
     mov [bx], al
+    mov ah, 0x0e
+    mov al, cl
+    int 0x10
     inc bx
-    cmp al, 13
+    inc cx
+    cmp cx, 4
     je go
     jmp input
 go:
     mov bx, string
     mov ah, 0x0e
+    mov cl, 49
 print:
     mov al, [bx]
     cmp al, 0
     je exit
     int 0x10
+    mov al, cl
+    int 0x10
     inc bx
+    inc cx
     jmp print
 
-loop:
-    mov ah, 0x00
-    int 0x16         
-    mov ah, 0x0e
-    int 0x10
-    cmp al, 13
-    je exit
-    jmp loop
-
+; loop:
+;     mov ah, 0x00
+;     int 0x16         
+;     mov ah, 0x0e
+;     int 0x10
+;     cmp al, 13
+;     je exit
+;     jmp loop
+mov al, 40
+int 0x10
 exit:
     jmp $
 times 510-($-$$) db 0
