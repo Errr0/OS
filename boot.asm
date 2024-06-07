2b2ad9c821517be6931e56ea19e607364e5389b2
Errr0 <fatalerror404.6@gmail.com>
2024-06-07 21:30:13 +0200

    reading from disk

--- boot.asm
+++ boot.asm
@@ -1,25 +1,19 @@
+diskNum:
+    db 0 
+    mov [diskNum], dl
+mov ah, 2
+mov al, 1
+mov ch, 0
+mov cl, 2
+mov dh, 0
+mov dl, [diskNum]
+mov bx, 0
+mov es, bx
+mov bx, 0x7e00
+int 0x13
 mov ah, 0x0e
-mov al, 65
+mov al, [0x7e00]
 int 0x10
-
-low:
-    inc al
-    cmp al, 91
-    je exit
-    add al, 32
-    int 0x10
-    jmp high
-
-
-high:
-    sub al, 32
-    inc al
-    cmp al, 91
-    je exit
-    int 0x10
-    jmp low
-
-exit:
-    jmp $
+jmp $
 times 510-($-$$) db 0
 db 0x55, 0xaa
