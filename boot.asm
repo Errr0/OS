5f61ce0d696d99f4058204577063027d15d4e81f
Errr0 <fatalerror404.6@gmail.com>
2024-06-07 21:54:24 +0200

    protected mode

--- boot.asm
+++ boot.asm
@@ -1,19 +1,47 @@
-diskNum:
-    db 0 
-    mov [diskNum], dl
-mov ah, 2
-mov al, 1
-mov ch, 0
-mov cl, 2
-mov dh, 0
-mov dl, [diskNum]
-mov bx, 0
-mov es, bx
-mov bx, 0x7e00
-int 0x13
-mov ah, 0x0e
-mov al, [0x7e00]
-int 0x10
+GDT_Start:
+    null_descriptor:
+        dd 0
+        dd 0
+    code_descriptor:
+        dw 0xffff
+        dw 0
+        dd 0
+        db 10011010
+        db 11001111
+        db 0
+    data_descriptor:
+        dw 0xffff
+        dw 0
+        dd 0
+        db 10010010
+        db 11001111
+        db 0
+GDT_End:
+    
+GDT_Descriptor:
+    dw GDT_End - GDT_Start - 1
+    dd GDT_Start
+
+CODE_SEG equ code_descriptor - GDT_Start
+DATA_SEG equ data_descriptor - GDT_Start
+
+cli
+lgdt [GDT_Descriptor]
+
+mov eax, cr0
+or eax, 1
+mov cr0, eax; yay, 32 bit mode!!
+; far jump (jmp to other segment)
+jmp CODE_SEG:start_protected_mode
+
+[bits 32]
+start_protected_mode:
+    ; videoMemory = oxb8000
+    ; first  byte: char
+    ; second byte: colour
+    mov al, 'A'
+    mov ah, 0x0f ; white
+    mov [0xb8000], ax
 jmp $
 times 510-($-$$) db 0
 db 0x55, 0xaa
